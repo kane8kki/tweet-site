41ad7b32e4c8c06c7b4c71e24b305474633d8241
kane8kki <128425162+kane8kki@users.noreply.github.com>
2023-11-10 18:59:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
+// Importez les modules Firebase nécessaires
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-app.js";
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-database.js";
+
+// Configurer votre application Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyA8yeNUorIIlttsqEYISsGYYWojYt2rHtc",
+  authDomain: "my-awesome-site-c48bb.firebaseapp.com",
+  projectId: "my-awesome-site-c48bb",
+  storageBucket: "my-awesome-site-c48bb.appspot.com",
+  messagingSenderId: "859728588693",
+  appId: "1:859728588693:web:b7e90954682703fac9909c"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Récupérer une référence à la base de données
+const database = getDatabase(firebaseApp);
+const tweetsRef = ref(database, 'tweets');
+
 function sendTweet() {
   var pseudo = document.getElementById("pseudo").value;
   var content = document.getElementById("tweet-content").value;
 
   if (pseudo && content) {
-    var tweetList = document.getElementById("tweet-list");
-
-    var tweetElement = document.createElement("div");
-    tweetElement.className = "tweet";
-    tweetElement.innerHTML = `<strong>${pseudo}:</strong> ${content}`;
-
-    tweetList.appendChild(tweetElement);
+    // Envoyer le tweet à la base de données Firebase
+    push(tweetsRef, { pseudo: pseudo, content: content });
 
     // Clear input fields after tweeting
     document.getElementById("pseudo").value = "";
     document.getElementById("tweet-content").value = "";
   }
 }
+
+// Écouter les changements dans la base de données et mettre à jour l'affichage
+onValue(tweetsRef, (snapshot) => {
+  var tweetList = document.getElementById("tweet-list");
+  tweetList.innerHTML = '';
+
+  snapshot.forEach((childSnapshot) => {
+    var tweet = childSnapshot.val();
+    var tweetElement = document.createElement("div");
+    tweetElement.className = "tweet";
+    tweetElement.innerHTML = `<strong>${tweet.pseudo}:</strong> ${tweet.content}`;
+
+    tweetList.appendChild(tweetElement);
+  });
+});
